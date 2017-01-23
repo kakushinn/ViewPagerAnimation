66cb9783a4abec6d2bca5676f2ccf07c06086c13
guochen <867625670@qq.com>
2017-01-23 16:30:56 +0800

    轮播控件放置recyclerView中
    轮播控件放置recyclerView中

--- ViewPagerAnimation/app/src/main/java/Logic/NewsListAdapter.java
+++ ViewPagerAnimation/app/src/main/java/Logic/NewsListAdapter.java
@@ -2,14 +2,22 @@ package Logic;
 
 import android.content.ClipData;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.guochen.viewpageranimation.NewsImageViewPager;
 import com.example.guochen.viewpageranimation.R;
 
 import java.util.List;
@@ -25,6 +33,7 @@ public class NewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     private LayoutInflater inflater;
     private MyRecyclerViewItemClickListener mListener;
     private ImageLoader imageLoader;
+    public int currentIndex = 0;
     public interface MyRecyclerViewItemClickListener{
         public void onItemClick(View view, int position);
     }
@@ -41,15 +50,32 @@ public class NewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        ItemViewHolder viewHolder;
-        View view = inflater.from(context).inflate(R.layout.news_list_item,null);
-        viewHolder = new ItemViewHolder(view, mListener);
-        return viewHolder;
+    public int getItemViewType(int position) {
+        if(position == 0){
+            return 0;
+        }else{
+            return 1;
+        }
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        if(i == 0){
+            FirstItemViewHolder firstViewHolder;
+            View view = inflater.from(context).inflate(R.layout.first_news_list_item,null);
+            firstViewHolder = new FirstItemViewHolder(view);
+            return firstViewHolder;
+        }else{
+            ItemViewHolder viewHolder;
+            View view = inflater.from(context).inflate(R.layout.news_list_item,null);
+            viewHolder = new ItemViewHolder(view, mListener);
+            return viewHolder;
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, int i) {
         if(viewHolder instanceof ItemViewHolder){
             String url = list.get(i).getThumbnail_pic();
             ((ItemViewHolder)viewHolder).newsImageView.setTag(url);
@@ -57,6 +83,77 @@ public class NewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             imageLoader.loadImage(((ItemViewHolder) viewHolder).newsImageView, url);
             ((ItemViewHolder)viewHolder).newsTitleText.setText(list.get(i).getTitle());
             ((ItemViewHolder)viewHolder).newsDateText.setText(list.get(i).getDate().substring(0,10));
+        }else if(viewHolder instanceof FirstItemViewHolder){
+            ((FirstItemViewHolder)viewHolder).initTopImageDots();
+            ((FirstItemViewHolder)viewHolder).newsImageViewPager.setAdapter(new PagerAdapter() {
+                @Override
+                public int getCount() {
+                    if (list == null || list.size() == 0 || list.size() >= 3)
+                        return 3;
+                    else
+                        return list.size();
+                }
+
+                @Override
+                public boolean isViewFromObject(View view, Object object) {
+                    return view == object;
+                }
+
+                @Override
+                public Object instantiateItem(ViewGroup container, final int position) {
+                    ImageView imageView = new ImageView(context);
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    if (position == 0)
+                        imageView.setImageResource(R.mipmap.guide_image1);
+                    else if (position == 1)
+                        imageView.setImageResource(R.mipmap.guide_image2);
+                    else
+                        imageView.setImageResource(R.mipmap.guide_image3);
+                    if (list != null && list.size() != 0) {
+                        ImageLoader imageLoader = new ImageLoader();
+                        String imageUrl = list.get(position).getThumbnail_pic_large();
+                        imageView.setTag(imageUrl);
+                        imageView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent();
+                                intent.setAction("android.intent.action.VIEW");
+                                Uri url = Uri.parse(list.get(position).getUrl());
+                                intent.setData(url);
+                                context.startActivity(intent);
+                            }
+                        });
+                        imageLoader.loadImage(imageView, imageUrl);
+                    }
+                    container.addView(imageView);
+                    return imageView;
+                }
+
+                @Override
+                public void destroyItem(ViewGroup container, int position, Object object) {
+                    container.removeView((ImageView) object);
+                }
+            });
+
+            ((FirstItemViewHolder)viewHolder).newsImageViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                }
+
+                @Override
+                public void onPageSelected(final int position) {
+                    ((FirstItemViewHolder)viewHolder).dots[position].setImageResource(R.mipmap.white_dot);
+                    ((FirstItemViewHolder)viewHolder).dots[currentIndex].setImageResource(R.mipmap.dark_dot);
+                    currentIndex = position;
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+
+                }
+            });
+            ((FirstItemViewHolder)viewHolder).mNewsImageHandler.sendEmptyMessageDelayed(0, 3000);
         }
     }
 
@@ -82,6 +179,49 @@ public class NewsListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         public void onClick(View v) {
             if(itemListener != null){
                 itemListener.onItemClick(v,getPosition());
+            }
+        }
+    }
+
+    class FirstItemViewHolder extends RecyclerView.ViewHolder
+    {
+        public NewsImageViewPager newsImageViewPager;
+        public LinearLayout dotsLinearLayout;
+        public ImageView[] dots = new ImageView[3];
+        public NewsImageHandler mNewsImageHandler = new NewsImageHandler();
+
+        public FirstItemViewHolder(View itemView) {
+            super(itemView);
+            newsImageViewPager = (NewsImageViewPager)itemView.findViewById(R.id.newsImageViewPager);
+            dotsLinearLayout = (LinearLayout)itemView.findViewById(R.id.topImageLayout);
+        }
+
+        public void initTopImageDots()
+        {
+            for(int i = 0;i<dotsLinearLayout.getChildCount();i++)
+            {
+                dots[i] = (ImageView)(dotsLinearLayout.getChildAt(i));
+            }
+            currentIndex = 0;
+            dots[0].setImageResource(R.mipmap.white_dot);
+        }
+
+        class NewsImageHandler extends Handler
+        {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what)
+                {
+                    case 0:
+                        int preIndex = newsImageViewPager.getCurrentItem();
+                        currentIndex = (currentIndex+1)%3;
+                        newsImageViewPager.setCurrentItem(currentIndex);
+                        dots[currentIndex].setImageResource(R.mipmap.white_dot);
+                        dots[preIndex].setImageResource(R.mipmap.dark_dot);
+                        sendEmptyMessageDelayed(0,3000);
+                        break;
+                }
             }
         }
     }
--- ViewPagerAnimation/app/src/main/java/View/Animation/HotNewsFragment.java
+++ ViewPagerAnimation/app/src/main/java/View/Animation/HotNewsFragment.java
@@ -39,33 +39,10 @@ import Logic.TopNewsService;
 public class HotNewsFragment extends android.support.v4.app.Fragment implements NewsListAdapter.MyRecyclerViewItemClickListener{
 
     public RecyclerView newsListView;
-    public NewsImageViewPager viewpager;
     public List<News> _list;
     public NewsListAdapter.MyRecyclerViewItemClickListener myRecyclerViewItemClickListener = null;
     public SwipeRefreshLayout swipeRefreshLayout = null;
     public LinearLayout imageLinearLayout;
-    public ImageView[] dots = new ImageView[3];
-    public int currentIndex = 0;
-    public NewsImageHandler mNewsImageHandler = new NewsImageHandler();
-
-    class NewsImageHandler extends Handler
-    {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what)
-            {
-                case 0:
-                    int preIndex = viewpager.getCurrentItem();
-                    currentIndex = (currentIndex+1)%3;
-                    viewpager.setCurrentItem(currentIndex);
-                    dots[currentIndex].setImageResource(R.mipmap.white_dot);
-                    dots[preIndex].setImageResource(R.mipmap.dark_dot);
-                    sendEmptyMessageDelayed(0,3000);
-                    break;
-            }
-        }
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,13 +56,12 @@ public class HotNewsFragment extends android.support.v4.app.Fragment implements 
         View view = inflater.from(getActivity()).inflate(R.layout.news_screen, null);
         swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.refreshLayout);
         newsListView = (RecyclerView)view.findViewById(R.id.news_list);
-        viewpager = (NewsImageViewPager)view.findViewById(R.id.newsImageViewPager);
         imageLinearLayout = (LinearLayout)view.findViewById(R.id.topImageLayout);
         newsListView.setHasFixedSize(true);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         newsListView.setLayoutManager(mLayoutManager);
         newsListView.setItemAnimator(new DefaultItemAnimator());
-        initTopImageDots();
+
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -93,29 +69,9 @@ public class HotNewsFragment extends android.support.v4.app.Fragment implements 
 
             }
         });
-        viewpager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                dots[position].setImageResource(R.mipmap.white_dot);
-                dots[currentIndex].setImageResource(R.mipmap.dark_dot);
-                currentIndex = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-        mNewsImageHandler.sendEmptyMessageDelayed(0,3000);
         new HotNewsAsyncTask().execute("http://v.juhe.cn/toutiao/index?type=tiyu&key=b69cc2e92edc5b582eba0a94c51173c8");
         return view;
     }
-
 
     class HotNewsAsyncTask extends AsyncTask<String,Void,List<News>>{
         @Override
@@ -134,56 +90,6 @@ public class HotNewsFragment extends android.support.v4.app.Fragment implements 
                 adapter.setOnItemClickListener(myRecyclerViewItemClickListener);
                 newsListView.setAdapter(adapter);
                 swipeRefreshLayout.setRefreshing(false);
-
-                viewpager.setAdapter(new PagerAdapter() {
-                    @Override
-                    public int getCount() {
-                        if(newses == null || newses.size() == 0 || newses.size()>=3)
-                            return 3;
-                        else
-                            return newses.size();
-                    }
-
-                    @Override
-                    public boolean isViewFromObject(View view, Object object) {
-                        return view == object;
-                    }
-
-                    @Override
-                    public Object instantiateItem(ViewGroup container, final int position) {
-                        ImageView imageView = new ImageView(getContext());
-                        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-                        if(position == 0)
-                            imageView.setImageResource(R.mipmap.guide_image1);
-                        else if(position == 1)
-                            imageView.setImageResource(R.mipmap.guide_image2);
-                        else
-                            imageView.setImageResource(R.mipmap.guide_image3);
-                        if(newses != null && newses.size() != 0){
-                            ImageLoader imageLoader = new ImageLoader();
-                            String imageUrl = newses.get(position).getThumbnail_pic_large();
-                            imageView.setTag(imageUrl);
-                            imageView.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    Intent intent = new Intent();
-                                    intent.setAction("android.intent.action.VIEW");
-                                    Uri url = Uri.parse(newses.get(position).getUrl());
-                                    intent.setData(url);
-                                    startActivity(intent);
-                                }
-                            });
-                            imageLoader.loadImage(imageView, imageUrl);
-                        }
-                        container.addView(imageView);
-                        return imageView;
-                    }
-
-                    @Override
-                    public void destroyItem(ViewGroup container, int position, Object object) {
-                        container.removeView((ImageView)object);
-                    }
-                });
             }
         }
     }
@@ -195,15 +101,5 @@ public class HotNewsFragment extends android.support.v4.app.Fragment implements 
         Uri url = Uri.parse(_list.get(position).getUrl());
         intent.setData(url);
         startActivity(intent);
-    }
-
-    public void initTopImageDots()
-    {
-        for(int i = 0;i<imageLinearLayout.getChildCount();i++)
-        {
-            dots[i] = (ImageView)(imageLinearLayout.getChildAt(i));
-        }
-        currentIndex = 0;
-        dots[0].setImageResource(R.mipmap.white_dot);
     }
 }
